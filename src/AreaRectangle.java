64e61f7f746769d8cc17a648c87dd39dc01b96f4
yordanpetkov1990 <yordanpetkov1990@gmail.com>
2023-03-07 16:44:23 +0200

    initial commit

---
+++ src/AreaRectangle.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class AreaRectangle {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int area_of_rectangle = a*b;
+        System.out.println(area_of_rectangle);
+    }
+}
---
+++ src/Zoo.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Zoo {
+    public static void main(String[] args) {
+        Scanner scan =new Scanner(System.in);
+        int dogs = Integer.parseInt(scan.nextLine());
+        int cats = Integer.parseInt(scan.nextLine());
+        double sum = dogs * 2.50 + cats * 4;
+        System.out.println(sum + " lv.");
+    }
+}
